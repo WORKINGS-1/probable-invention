729d3164cf71514426b4786c478685d7408276d0
WORKINGS-1 <shaibotbol@yahoo.fr>
2025-05-27 15:11:03 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
-// À compléter avec ta config Firebase plus tard
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "TON_DOMAINE.firebaseapp.com",
-  projectId: "TON_PROJECT_ID",
-  storageBucket: "TON_BUCKET",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyB9pUM91EE6cO52GHLsGA2uGS_X5qkRygo",
+  authDomain: "maasser-app.firebaseapp.com",
+  projectId: "maasser-app",
+  storageBucket: "maasser-app.firebasestorage.app",
+  messagingSenderId: "591588212431",
+  appId: "1:591588212431:web:7a37853109e1f0887181ba"
 }
 
 const app = initializeApp(firebaseConfig)
+
 export const auth = getAuth(app)
 export const db = getFirestore(app)
